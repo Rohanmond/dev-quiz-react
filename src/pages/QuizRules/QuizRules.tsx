16502287d78cd83b7c905d6ee267c63c5218c05c
Rohan Mondal <mondalrohan108@gmail.com>
2022-04-23 07:32:53 +0530

    fixed quiz rules

--- src/pages/QuizRules/QuizRules.tsx
+++ src/pages/QuizRules/QuizRules.tsx
@@ -10,21 +10,26 @@ export const QuizRules = () => {
   return (
     <div className='home-container'>
       <NavBar />
-      <div className='quiz-rules-header'>
-        <h1>Quiz Rules</h1>
-      </div>
-      <Link to={'/categories'} className='go-back-btn font-wt-semibold text-md'>
-        Go Back
-      </Link>
-      <div className='quiz-rules-content brd-rd-semi-sq'>
-        <div className='rules-details'>
-          <p>There are a total of {QuizData?.points} questions</p>
-          <p>Each questions is of 10 points</p>
-          <p>You get 60 seconds to answer each question</p>
+      <div className='quiz-rules-container'>
+        <div className='quiz-rules-header'>
+          <h1>Quiz Rules</h1>
         </div>
-        <Link to={`/${quizId}/1`} className='start-quiz-btn font-wt-semibold'>
-          Start Quiz
+        <Link
+          to={'/categories'}
+          className='go-back-btn font-wt-semibold text-md'
+        >
+          Go Back
         </Link>
+        <div className='quiz-rules-content brd-rd-semi-sq'>
+          <div className='rules-details'>
+            <p>There are a total of {QuizData?.points} questions</p>
+            <p>Each questions is of 10 points</p>
+            <p>You get 60 seconds to answer each question</p>
+          </div>
+          <Link to={`/${quizId}/1`} className='start-quiz-btn font-wt-semibold'>
+            Start Quiz
+          </Link>
+        </div>
       </div>
     </div>
   );
