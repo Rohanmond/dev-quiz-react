6eb70572834c770c0db924bc738aa7835790bb41
Rohan Mondal <mondalrohan108@gmail.com>
2022-05-09 18:44:42 +0530

    deleted unwanted link

--- src/pages/Auth/Signup.tsx
+++ src/pages/Auth/Signup.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/auth-context';
 import {
   validateEmail,
